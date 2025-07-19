e73972b1de2535f2fa40f3aea0949ee013473048
SalaiThomas <salaithomas582@gmail.com>
2025-07-19 22:20:36 +0630

    add custom-button in view

---
+++ pos_custom_button/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Custom Button",
+
+    'summary': "To Practice And Learning Purpose",
+
+    'description': """To Practice And Learning Purpose""",
+
+    'author': "Salai Thomas",
+
+    'website': "https://github.com/Salai-Thomas",
+
+    'category': 'Uncategorized',
+
+    'version': "18.0.0.0",
+
+    'depends': ['base','point_of_sale'],
+
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_custom_button/static/src/xml/custom_button.xml',
+        ],
+    },
+}
+
